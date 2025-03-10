6e09fa31892a050e4c3a0d11c36176c7d0eeece4
Aggelos Sakalis <aggelossakalis8@gmail.com>
2025-03-10 18:49:34 +0200

    backend

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const {
     createNote, 
     getNotesByUser, 
     deleteNote 
-} = require('./controllers/userController.js');  // Ensure correct path
+} = require('./controllers/userController');  // Ensure correct path
 
 
 // JWT secret
