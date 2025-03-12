4e6816324d88e89fa40bcae873608086dc093f7b
Aggelos Sakalis <aggelossakalis8@gmail.com>
2025-03-12 18:52:28 +0200

    add Modify and Delete Options

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,15 @@ const exampleController = require('./controllers/exampleController');
 
 // User routes
 router.post('/users', exampleController.createUser); // Sign up
-router.post('/login', exampleController.loginUser); // Login
 router.get('/users', exampleController.getUsers); // Get all users
 
 // Note routes
 router.post('/notes', exampleController.createNote); // Create a new note
 router.get('/notes', exampleController.getNotesByUser); // Get notes for logged-in user
 router.post('/login', exampleController.loginUser);  // Add this route
+router.delete('/notes/:id', exampleController.deleteNote); // Add this route
+router.put('/notes/:id', exampleController.ModifyNote); // Add this route
+
 
 
 module.exports = router;
